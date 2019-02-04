b98b9996c453d4dfa5e38114dbe5057efdadaf37
root <root@hrms001.healthians.co.in>
2019-02-04 17:27:56 +0530

    initial commit

---
+++ public/db_constants.php
@@ -0,0 +1,7 @@
+<?php
+	           defined('SENTRIFUGO_HOST') || define('SENTRIFUGO_HOST','rds002.healthians.co.in');
+	           defined('SENTRIFUGO_USERNAME') || define('SENTRIFUGO_USERNAME','sentrifugo');
+	           defined('SENTRIFUGO_PASSWORD') || define('SENTRIFUGO_PASSWORD','Zbroglytz6dszdJsnb');
+	           defined('SENTRIFUGO_DBNAME') || define('SENTRIFUGO_DBNAME','sentrifugo');
+	           
+	         ?>
---
+++ public/mail_settings_constants.php
@@ -0,0 +1,8 @@
+<?php
+	           defined('MAIL_SMTP') || define('MAIL_SMTP','smtp.mandrillapp.com');
+	           defined('MAIL_USERNAME') || define('MAIL_USERNAME','deepak@healthians.com');
+	           defined('MAIL_PASSWORD') || define('MAIL_PASSWORD','mAAUVwfBFDtLIR0g39dkfQ');
+	           defined('MAIL_PORT') || define('MAIL_PORT','587');
+	           defined('MAIL_AUTH') || define('MAIL_AUTH','true');
+	           defined('MAIL_TLS') || define('MAIL_TLS','');
+	         ?>
